3a2d2dde177d67c70c59b40725ed3ef7748de0ee
Nathan Davidov <NathanDavidov9@gmail.com>
2025-06-17 13:00:17 -0400

    access B files seperately, plot first 20 elements of a given sequence

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 function Homepage() {
-    const [msg, changeMsg] = useState("A000045");
+    const [msg, changeMsg] = useState("000045");
     const [txt, changeTxt] = useState("");
+    const [img, changeImg] = useState();
 
     function handleMsgChange(e) {
         changeMsg(e.target.value);
@@ -14,11 +15,28 @@ function Homepage() {
             const response = await fetch(url);
             const json = await response.json();
             changeTxt(json);
-
             console.log(json);
         } catch (error) {
             console.log(error.message);
         }
+    }
+
+    async function getDataB() {
+        const url = `http://localhost:8000/seqB?seqID=${msg}`;
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            changeTxt(json);
+            console.log(json);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async function getPicture() {
+        const url = `http://localhost:8000/getPic?seqID=${msg}`;
+        changeImg(url);
+        console.log(url);
     }
 
     return (
@@ -29,7 +47,13 @@ function Homepage() {
                     value={msg}
                     onChange={handleMsgChange}
                 ></input>
-                <button onClick={getData}>get data</button>
+                <button onClick={getData}>get (A) File</button>
+                <button onClick={getDataB}>get (B) File</button>
+                <button onClick={getPicture}>Get Picture</button>
+
+                <div>
+                    <img src={img}></img>
+                </div>
             </div>
             <div>{txt}</div>
         </div>
